0ead8a390873471d2788757650df56ea506efb4a
lilei <1339098678@qq.com>
2022-03-18 15:00:17 +0800

    edit

--- build/CMakeFiles/202112-1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 202112-1.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lilei/Documents/GitHub/CSP-Questions
-# Build directory: /Users/lilei/Documents/GitHub/CSP-Questions/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
