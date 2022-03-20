8bbe60cc4a3a81246ab012eccb6ec5daaa5a257a
lilei <37171564+xiaoleitongxue@users.noreply.github.com>
2022-03-20 16:03:39 +0800

    edit readme

--- test.cpp
+++ test.cpp
@@ -3,12 +3,5 @@
 using namespace std;
 int main(int, char **)
 {
-   vector<int> a;
-   int temp;
-   for(int i = 0; i < 3; ++i){
-       cin >> temp;
-       a.push_back(temp);
-   }
-   int n;
-   cin >> n;
+   cout << "Hello World!" << endl;
 }
