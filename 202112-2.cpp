218faa507d4d2ce5be31cee68dd955250f805bb0
lilei <37171564+xiaoleitongxue@users.noreply.github.com>
2022-04-13 11:21:45 +0800

    edit 202112-2.cpp

--- 202112-2.cpp
+++ 202112-2.cpp
@@ -10,34 +10,35 @@ using namespace std;
 int main(int, char**){
     int n, N;
     cin >> n >> N;
-    vector<int> nums(n+1, 0);
+    int nums[n+1] = {0};
     for(int i = 1; i < n + 1; ++i){
         cin >> nums[i];
     }
-    int r = N/(n + 1);
-    vector<int> f(N, 0);
-    vector<int> g(N, 0);
-    int j = 1;
-    for(int i = 1; i < N;){
-        if(j == n + 1){
-            f[i] = n;
-            ++i;
-        }else if(i < nums[j]){
-            f[i] = j - 1;
-            ++i;
-        }else if(i == nums[j]){
-            f[i] = j;
-            ++i;
+    int f[N] ={0};
+    int g[N] = {0};
+    int err[N] = {0};
+    int i = 0;
+    int j = 0;
+    while(i < N){
+        cout << i << ' ' << j << endl;
+        if(j < n + 1){
+            if(i < nums[j]){
+                f[i] = j - 1 >= 0 ? j - 1 : 0;
+                i++;
+            }else if(i == nums[j]){
+                j++;
+            }else{
+                j++;
+            }
         }else{
-            ++j;
+            f[i++] = n;
         }
     }
+    cout << "here" << endl;
+    int r = N/(n+1);
+    int sum = 0;
     for(int i = 0; i < N; ++i){
-        g[i] = i / r;
+        err[i] = abs(i / r - f[i]);
     }
-    vector<int> errors(N, 0);
-    for(int i = 0; i < N; ++i){
-        errors[i] = abs(g[i] - f[i]);
-    }
-    cout << accumulate(errors.begin(), errors.end(), 0);
+    cout << sum;
 }
